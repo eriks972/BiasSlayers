0d6e37f03ded49154253af3aa571bb9dc2f41370
Erik Thomas Swanson <48566358+eriks972@users.noreply.github.com>
2026-05-06 13:17:04 -0400

    Update API URL to use environment variable

--- biasslayers-ui/app/page.tsx
+++ biasslayers-ui/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   const analyze = async () => {
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict_all`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ text }),
-      });
+     });
 
       const data = await res.json();
       setResult(data);
